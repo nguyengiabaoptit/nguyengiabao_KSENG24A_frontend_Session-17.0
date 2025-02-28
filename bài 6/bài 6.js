b997f400ed6b1db07a253fafee87c5390fcbca95
nguyengiabaoptit <nguyengiabao2880@gmail.com>
2025-02-28 08:20:44 +0700

    upload bài 6

---
+++ bài 6/bài 6.js
@@ -0,0 +1,13 @@
+let str1 = "5";
+let str2 = "-1";
+let str3 = "Hello";
+console.log(typeof(Number(str1)));
+console.log(typeof(Number(str2)));
+console.log(typeof(Number(str3)));
+
+let num1 = 55;
+let num2 = -10;
+let num3 = 0;
+console.log(typeof(String(num1)));
+console.log(typeof(String(num2)));
+console.log(typeof(String(num3)));
